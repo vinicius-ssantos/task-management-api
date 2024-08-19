b704764bc08a84d1ff18ad1a4bb1d05c87ada518
Vinicius <viniciusolicveira@gmail.com>
2024-08-19 13:55:53 -0300

    feat(entity): configurar o TypeORM + PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ConfigModule } from '@nestjs/config';
     UsersModule,
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    DbModule,
   ],
   controllers: [AppController],
   providers: [AppService],
